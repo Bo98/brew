f5fcb0a46a0cb26c0e0717451525a2f3787ad718
Bo Anderson <mail@boanderson.me>
2020-06-10 16:10:33 +0100

    language/java: add support for OpenJDK

--- Library/Homebrew/language/java.rb
+++ Library/Homebrew/language/java.rb
@@ -2,7 +2,31 @@
 
 module Language
   module Java
+    def self.find_openjdk_formula(version = nil)
+      can_be_newer = version&.end_with?("+")
+      version = version.to_i
+
+      openjdk = Formula["openjdk"]
+      [openjdk, openjdk.versioned_formulae].flatten.find do |f|
+        next false unless f.any_version_installed?
+
+        if version != 0
+          major = f.version.to_s[/\d+/].to_i
+          next false unless major >= version
+          next false unless major == version || can_be_newer
+        end
+
+        true
+      end
+    rescue FormulaUnavailableError
+      nil
+    end
+    private_class_method :find_openjdk_formula
+
     def self.java_home(version = nil)
+      f = find_openjdk_formula(version)
+      return f.opt_libexec if f
+
       req = JavaRequirement.new [*version]
       raise UnsatisfiedRequirements, req.message unless req.satisfied?
 
